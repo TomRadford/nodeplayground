15d8342a27682c202bb0929f4dc3d8bd4f664045
Tom Radford <tom@theradford.com>
2022-04-10 11:34:12 +0200

    up to 4a

--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument')
-    process.exit()
+  console.log('Please provide the password as an argument')
+  process.exit()
 }
 
 const password = process.argv[2]
@@ -10,21 +10,21 @@ const password = process.argv[2]
 const url =
  `mongodb+srv://fullstack:${password}@fullstack.qrmkt.mongodb.net/noteApp?retryWrites=true&w=majority`
 
- mongoose.connect(url);
+mongoose.connect(url)
 
- const noteSchema = new mongoose.Schema({
-     content: String,
-     date: Date,
-     important: Boolean
- })
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean
+})
 
- const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model('Note', noteSchema)
 
 Note.find().then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
 })
 
 
